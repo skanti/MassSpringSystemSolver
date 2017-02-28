d13039161f5948c7775dd3bfc01d39597f995aeb
Armen Avetisyan <armen.avetisyan.to@gmail.com>
2017-02-28 11:49:56 +0100

    Preparing cloth example for testing.

--- src/MSNWorld.cpp
+++ src/MSNWorld.cpp
@@ -7,11 +7,9 @@
 #include "Timer.h"
 
 const double dt = 1.0/60.0;
-#define N_MAX_NODES 500
-#define N_MAX_SPRINGS 500
-#define N_PT 13
-#define N_PITCH 3
-#define N_LENGTH 2
+#define N_MAX_NODES 1000
+#define N_MAX_SPRINGS 2000
+#define N_CLOTH 20
 
 MSNWorld::MSNWorld() : World() {
     flag = 1;
@@ -20,12 +18,6 @@ MSNWorld::MSNWorld() : World() {
     // -> reserve memory
     nodes.reserve(N_MAX_NODES);
     springs.reserve(N_MAX_SPRINGS);
-    T0.resize(N_PT);
-    T1.resize(N_PT + 1);
-    T2.resize(N_PT);
-    S0.resize(N_MAX_NODES);
-    V0.resize(N_MAX_NODES);
-    // S1.resize(13 + 3);
     A.reserve(2*N_MAX_SPRINGS);
     A.resize(N_MAX_NODES, N_MAX_SPRINGS);
     J.reserve(2*N_MAX_SPRINGS);
@@ -42,26 +34,18 @@ MSNWorld::MSNWorld() : World() {
 
     // -> load and set mesh
     //load_mesh_ply2<float>("/dtome/amon/grive/development/MassSpringNetwork/data/canstick.ply2", nodes, springs);
-    create_microtubule<double>(nodes, springs, A, T0, T1, S0, V0, L, N_PT, N_PITCH, N_LENGTH);
+    create_cloth<double>(nodes, springs, A, N_CLOTH);
     normalize_and_recenter_nodes<double>(nodes);
     springs.set_as_equilibrium(nodes.px, nodes.py, nodes.pz, A);
-    T1[0] = 0;
-    std::partial_sum(&T0[0], &T0[0] + N_PT, &T1[1], std::plus<int>());
-    for (int i = 0, j = N_LENGTH*2 - 1; i < N_PT; i++, j += N_LENGTH*2) T2[i] = j; 
     // <-
 
     M.setIdentity();
-    for (int i = 0, j0 = 0; i < N_PT; i++, j0 += T0[i]) {
-        for (int j = 0; j < N_PITCH; j++) {
-        int k = j + j0;
-        M.valuePtr()[k] = 1e6;
-        }
-    }
+    M.valuePtr()[0] = 1e6;
+    M.valuePtr()[N_CLOTH - 1] = 1e6;
 
     mt.seed(999);
-    dist_int_uniform = std::uniform_int_distribution<int>(0, N_PT - 1);
 
-    f_gravity = Eigen::MatrixXd::Constant(N_MAX_NODES, 1, 0.01f);
+    f_gravity = Eigen::MatrixXd::Constant(N_MAX_NODES, 1, 0.001f);
     fx_langevin = Eigen::MatrixXd::Constant(N_MAX_NODES, 1, 0);
     fy_langevin = Eigen::MatrixXd::Constant(N_MAX_NODES, 1, 0);
     fz_langevin = Eigen::MatrixXd::Constant(N_MAX_NODES, 1, 0);
@@ -88,7 +72,7 @@ MSNWorld &MSNWorld::get_instance() {
 void MSNWorld::init_shape() {
     glm::mat4 trans_mat = glm::translate(glm::mat4(1), glm::vec3(0, 0, 0));
     vao.model_matrix = trans_mat*glm::scale(glm::mat4(1), glm::vec3(zoom));
-    vao.shape = ga::Shape::make_sphere(0.02f);
+    vao.shape = ga::Shape::make_sphere(0.01f);
 }
 
 void MSNWorld::init_shader() {
@@ -196,18 +180,16 @@ void MSNWorld::draw() {
 };
 
 void MSNWorld::gather_for_rendering() {
-    std::vector<int> t(N_PT, 0);
 
-    for (int i = 0, j0 = 0; i < N_PT; j0 += T0[i], i++) {
-        for (int j = 0; j < T0[i]; j++) {
-            int k = j + j0;
+    for (int i = 0; i < N_CLOTH; i++) {
+        for (int j = 0; j < N_CLOTH; j++) {
+            int k = i*N_CLOTH + j;
             vao.p_xyz[k*3 + 0] = (float)nodes.px[k];
             vao.p_xyz[k*3 + 1] = (float)nodes.py[k];
             vao.p_xyz[k*3 + 2] = (float)nodes.pz[k];
-            vao.color[k*3 + 0] = t[V0[k]] % 2 ? 0.2 : 0.9;
-            vao.color[k*3 + 1] = t[V0[k]] % 2 ? 0.2 : 0.9;
+            vao.color[k*3 + 0] = i%2 ? 0.2 : 0.9;
+            vao.color[k*3 + 1] = i%2 ? 0.2 : 0.9;
             vao.color[k*3 + 2] = 0.9;
-            t[V0[k]]++;
         }
 
     }
@@ -217,7 +199,7 @@ void MSNWorld::gather_for_rendering() {
         int a = J.innerIndexPtr()[pi0];
         int b = J.innerIndexPtr()[pi0 + 1];
         vao.s_ab[j*2] = a;
-        vao.s_ab[j*2+1] = b;
+        vao.s_ab[j*2 + 1] = b;
     } 
 };
 
@@ -231,6 +213,9 @@ void MSNWorld::advance(std::size_t &iteration_counter, long long int ms_per_fram
     }
     
     double h2 = dt*dt;
+
+    nodes.pz[0] = 0.2*std::sin(iteration_counter*5e-3);
+    nodes.pz[N_CLOTH - 1] = 0.2*std::sin(iteration_counter*5e-3);
 
     // std::generate(&fx_langevin[0], &fx_langevin[0] + nodes.n_size, [&](){return 0.01*dist_normal(mt);});
     // std::generate(&fy_langevin[0], &fy_langevin[0] + nodes.n_size, [&](){return 0.01*dist_normal(mt);});
@@ -249,7 +234,7 @@ void MSNWorld::advance(std::size_t &iteration_counter, long long int ms_per_fram
     nodes.py.block(0, 0, nodes.n_size, 1) = nodes.qy.block(0, 0, nodes.n_size, 1);
     nodes.pz.block(0, 0, nodes.n_size, 1) = nodes.qz.block(0, 0, nodes.n_size, 1);
 
-    for (int i = 0; i < 10; i++) {
+    for (int i = 0; i < 20; i++) {
         springs.dx_rhs.block(0, 0, springs.n_size, 1) = (nodes.px.block(0, 0, nodes.n_size, 1).transpose()*J.block(0, 0, nodes.n_size, springs.n_size)).transpose();
         springs.dy_rhs.block(0, 0, springs.n_size, 1) = (nodes.py.block(0, 0, nodes.n_size, 1).transpose()*J.block(0, 0, nodes.n_size, springs.n_size)).transpose();
         springs.dz_rhs.block(0, 0, springs.n_size, 1) = (nodes.pz.block(0, 0, nodes.n_size, 1).transpose()*J.block(0, 0, nodes.n_size, springs.n_size)).transpose();
@@ -264,7 +249,7 @@ void MSNWorld::advance(std::size_t &iteration_counter, long long int ms_per_fram
         nodes.px_rhs.block(0, 0, nodes.n_size, 1) = M.block(0, 0, nodes.n_size, nodes.n_size)*nodes.qx.block(0, 0, nodes.n_size, 1) + h2*(J.block(0, 0, nodes.n_size, springs.n_size)*springs.dx.block(0, 0, springs.n_size, 1) 
             + fx_langevin.block(0, 0, nodes.n_size, 1));
         nodes.py_rhs.block(0, 0, nodes.n_size, 1) = M.block(0, 0, nodes.n_size, nodes.n_size)*nodes.qy.block(0, 0, nodes.n_size, 1) + h2*(J.block(0, 0, nodes.n_size, springs.n_size)*springs.dy.block(0, 0, springs.n_size, 1) 
-            + fy_langevin.block(0, 0, nodes.n_size, 1));
+            + fy_langevin.block(0, 0, nodes.n_size, 1) - f_gravity.block(0, 0, nodes.n_size, 1));
         nodes.pz_rhs.block(0, 0, nodes.n_size, 1) = M.block(0, 0, nodes.n_size, nodes.n_size)*nodes.qz.block(0, 0, nodes.n_size, 1) + h2*(J.block(0, 0, nodes.n_size, springs.n_size)*springs.dz.block(0, 0, springs.n_size, 1)
             + fz_langevin.block(0, 0, nodes.n_size, 1));
 
@@ -272,6 +257,11 @@ void MSNWorld::advance(std::size_t &iteration_counter, long long int ms_per_fram
         nodes.px.block(0, 0, nodes.n_size, 1) = chol.solve(nodes.px_rhs.block(0, 0, nodes.n_size, 1));
         nodes.py.block(0, 0, nodes.n_size, 1) = chol.solve(nodes.py_rhs.block(0, 0, nodes.n_size, 1));
         nodes.pz.block(0, 0, nodes.n_size, 1) = chol.solve(nodes.pz_rhs.block(0, 0, nodes.n_size, 1));
+
+
+    nodes.pz[0] = 0.2*std::sin(iteration_counter*5e-3);
+    nodes.pz[N_CLOTH - 1] = 0.2*std::sin(iteration_counter*5e-3);
+
     
     }
     nodes.vx.block(0, 0, nodes.n_size, 1) = (nodes.px.block(0, 0, nodes.n_size, 1) - nodes.vx.block(0, 0, nodes.n_size, 1))/dt;
@@ -284,116 +274,19 @@ void MSNWorld::advance(std::size_t &iteration_counter, long long int ms_per_fram
 }
 
 void MSNWorld::create_springs(int i_node) {
-    int j0 = (V0[i_node] - 1 + N_PT)%N_PT;
-    int j1 = (V0[i_node] + 1)%N_PT;
-    for (int k = T1[j1]; k < T1[j1 + 1]; k++) {
-        int a = i_node;
-        int b = S0[k];
-        double dxj = nodes.px[a] - nodes.px[b];
-        double dyj = nodes.py[a] - nodes.py[b];
-        double dzj = nodes.pz[a] - nodes.pz[b];
-        double d = std::sqrt(dxj*dxj + dyj*dyj + dzj*dzj);
-        if (d < 0.26) {
-            int64_t ab = a <= b ? ((int64_t)a << 32) + b : ((int64_t)b << 32) + a;
-            if (L.insert({ab, (unsigned char)1}).second) {
-                int l = J.outerIndexPtr()[springs.n_size];
-                J.innerIndexPtr()[l] = a;
-                J.innerIndexPtr()[l + 1] = b;
-                J.valuePtr()[l] = -1;
-                J.valuePtr()[l + 1] = 1;
-                springs.key[springs.n_size] = springs.n_size; 
-                springs.n_size++;
-                springs.set_as_equilibrium1(nodes.px, nodes.py, nodes.pz, J, springs.n_size - 1);
-            }
-        }
-    }
+
 }
 
 void MSNWorld::spawn_nodes(float px, float py) {
-    if (nodes.n_size < N_MAX_NODES && springs.n_size < N_MAX_SPRINGS) {
-        int i_protofilament = dist_int_uniform(mt);
 
-        int j = T2[i_protofilament];
-
-        // std::cout << "n_size " <<  nodes.n_size << std::endl;
-        // std::cout << "i_pf " <<  i_protofilament << std::endl;
-        // std::cout << "j " << j << std::endl;
-
-        double px0 = nodes.px[j];
-        double py0 = nodes.py[j] + 0.2;
-        double pz0 = nodes.pz[j];
-
-        nodes.set(nodes.n_size, px0, py0, pz0, 0, 0, 0, 1.0f);
-        S0[nodes.n_size] = nodes.n_size;
-        V0[nodes.n_size] = i_protofilament;
-        // std::cout << V0.block(0, 0, nodes.n_size + 1, 1) << std::endl;
-
-        // std::cout << S0.block(0, 0, nodes.n_size, 1) << std::endl;
-        // std::cout << "******" << std::endl;
-        nodes.insert(i_protofilament, N_PT, T0, S0);
-        nodes.n_size++;
-        T0[i_protofilament]++;
-        T2[i_protofilament] = nodes.n_size - 1;
-        std::partial_sum(&T0[0], &T0[0] + N_PT, &T1[1], std::plus<int>());
-
-
-        int l = J.outerIndexPtr()[springs.n_size];
-        int a = j;
-        int b = nodes.n_size - 1;
-        J.innerIndexPtr()[l] = a;
-        J.innerIndexPtr()[l + 1] = b;
-        J.valuePtr()[l] = -1;
-        J.valuePtr()[l + 1] = 1;
-        springs.key[springs.n_size] = springs.n_size;
-        int64_t ab = a <= b ? ((int64_t)a << 32) + b : ((int64_t)b << 32) + a;
-        L.insert({ab, (unsigned char)1});        
-        springs.n_size++;
-        springs.set_as_equilibrium1(nodes.px, nodes.py, nodes.pz, J, springs.n_size - 1);
-
-        create_springs(nodes.n_size - 1);
-
-        Q.leftCols(nodes.n_size) = M.block(0, 0, nodes.n_size, nodes.n_size) + (SparseMatrix<double>)(dt*dt*J.block(0, 0, nodes.n_size, springs.n_size)*J.block(0, 0, nodes.n_size, springs.n_size).transpose());
-        chol.compute(Q.block(0,0, nodes.n_size, nodes.n_size));
-    }
 }
 
 void MSNWorld::delete_nodes(float px, float py) {
-        
-        nodes.n_size--;
-
-        for (int i = J1.outerIndexPtr()[nodes.n_size]; i < J1.outerIndexPtr()[nodes.n_size + 1]; i++) {
-            springs.n_size--;
-            int a = springs.key[J1.innerIndexPtr()[i]];
-            int b = springs.n_size;
-            springs.swap(a, b, J);
-        }
-
-        // std::iota(&springs.key[0], &springs.key[0] + springs.n_size, 0);
-        Q.leftCols(nodes.n_size) = M.block(0, 0, nodes.n_size, nodes.n_size) + (SparseMatrix<double>)(dt*dt*J.block(0, 0, nodes.n_size, springs.n_size)*J.block(0, 0, nodes.n_size, springs.n_size).transpose());
-        chol.compute(Q.block(0,0, nodes.n_size, nodes.n_size));
-
-        // Eigen::IOFormat CleanFmt(2, 0, ", ", "\n", "[", "]");
-        // Eigen::Matrix<float, N_MAX_NODES, N_MAX_SPRINGS> J2 = J1;
-        // std::cout << J2.format(CleanFmt) << std::endl << std::endl;
-        // Eigen::Matrix<float, N_MAX_NODES, N_MAX_NODES> Q1 = Q;
-        // std::cout << Q1.format(CleanFmt) << std::endl << std::endl;
 
 }
+
 void MSNWorld::mouse_button_callback(GLFWwindow *window, int button, int action, int mods) {
-    // if (action == GLFW_PRESS) {
-    //     double px_target, py_target;
-    //     ga::Engine::get_instance().get_cursor_position(&px_target, &py_target);
-    //     px_target = px_target*2.0f / ga::WindowManager::width_window - 1.0f;
-    //     py_target = (-py_target*2.0f / ga::WindowManager::height_window + 1.0f)*ga::WindowManager::aspect_ratio_inv_window;
-    //     glm::mat4 z = glm::scale(glm::mat4(1), glm::vec3(get_instance().zoom));
-    //     glm::mat4 t = glm::translate(glm::mat4(1), glm::vec3(0, 0, 0));
-    //     glm::vec4 p = glm::inverse(z)*t*glm::vec4(px_target, py_target, 0, 1);
-    //     if (button == GLFW_MOUSE_BUTTON_LEFT) {
-    //         get_instance().spawn_nodes(p[0], p[1]);
-    //     } else if (button == GLFW_MOUSE_BUTTON_RIGHT) {
-    //         get_instance().delete_nodes(p[0], p[1]);
-    //     }
-    // }
+
 }
 
 
