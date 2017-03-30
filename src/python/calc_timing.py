ea635600e74d6682f3751cc2f55f0586bde132d7
Armen Avetisyan <armena@humungus.ee.ethz.ch>
2017-03-30 16:34:06 +0200

    Modyfing python timing plotter.

--- src/python/calc_timing.py
+++ src/python/calc_timing.py
@@ -2,15 +2,33 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 
-A = np.loadtxt("timing.dat", skiprows=1, usecols=[0, 1])
-A = A.reshape((2, 4, 2))
-B = A[1]
-A = A[0]
+A1 = np.loadtxt("./pool300-langevin0.0001/pd2-langevin0.000100-timing.dat", skiprows=1)
+A2 = np.loadtxt("./pool300-langevin0.0001/pd4-langevin0.000100-timing.dat", skiprows=1)
+A3 = np.loadtxt("./pool300-langevin0.0001/pd8-langevin0.000100-timing.dat", skiprows=1)
+A4 = np.loadtxt("./pool300-langevin0.0001/pd16-langevin0.000100-timing.dat", skiprows=1)
+A5 = np.loadtxt("./pool300-langevin0.0001/pd32-langevin0.000100-timing.dat", skiprows=1)
+A6 = np.loadtxt("./pool300-langevin0.0001/pd64-langevin0.000100-timing.dat", skiprows=1)
+A7 = np.loadtxt("./pool300-langevin0.0001/newton1-langevin0.000100-timing.dat", skiprows=1)
+A8 = np.loadtxt("./pool300-langevin0.0001/newton2-langevin0.000100-timing.dat", skiprows=1)
+A9 = np.loadtxt("./pool300-langevin0.0001/newton5-langevin0.000100-timing.dat", skiprows=1)
 
 
-plt.loglog(A[:, 0], A[:, 1], label="Newton")
-plt.loglog(B[:, 0], B[:, 1], label="variational")
+plt.subplot(1,2,1)
+plt.plot(A1, label="pd-2")
+plt.plot(A2, label="pd-4")
+plt.plot(A3, label="pd-8")
+plt.plot(A4, label="pd-16")
+plt.plot(A5, label="pd-32")
+plt.plot(A6, label="pd-64")
 plt.legend()
-# plt.savefig("error.png")
+plt.ylim(0, 0.5)
+
+plt.subplot(1,2,2)
+plt.plot(A7, label="newton-1")
+plt.plot(A8, label="newton-2")
+plt.plot(A9, label="newton-5")
+plt.legend()
+plt.ylim(0, 0.5)
+
 plt.show()
 #print(np.shape(C))
